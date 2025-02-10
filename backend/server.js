331fbc5f44c9fa772c0e308ffce548bd75bd3286
TheQueepingHour <redex109@gmail.com>
2025-02-09 18:11:44 -0600

    Successfully created server.js, connected to both database and listening for connections on port

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,40 @@
-//Modules
+//Config
 require('dotenv').config()
-
+//Dependencies
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const { Sequelize } = require('sequelize')
+const cors = require('cors')
 
-//Express Settings
+//Middleware & Express settings
+const PORT = process.env.PORT
+app.use(express.json())
 app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
+
+//Database Connection
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres'
+    }
+)
+
+sequelize.authenticate()
+    .then(() => console.log(`Database connection established successfully.`))
+    .catch(err => {
+        console.error(`Unable to connect to the database: ${err.message}`)
+        console.error(err)
+    })
 
 //Controllers/Routes
 
+
 //Listen for connections
-app.listen(process.env.PORT, () => {
-    console.log(`Now listening on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`Now listening on port: ${PORT}`)
 })
